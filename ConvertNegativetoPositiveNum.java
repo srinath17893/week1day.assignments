bca8b3514e88bda3d8f7fcd6e967ad48368d1d0e
srinath17893 <68018945+srinath17893@users.noreply.github.com>
2021-09-14 05:47:19 +0530

    Add files via upload

---
+++ ConvertNegativetoPositiveNum.java
@@ -0,0 +1,13 @@
+package week1.day1.assignments;
+
+public class ConvertNegativetoPositiveNum {
+
+	public static void main(String[] args) {
+		
+		int number = -40;
+			number=-number;
+			System.out.println("Converted to positive number: "+number);
+			
+	}
+
+}
